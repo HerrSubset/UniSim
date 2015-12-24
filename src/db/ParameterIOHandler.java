9d8179de666279d6685cacbce16c6eeb2f2110ed
HerrSubset <smets.pj@gmail.com>
2015-12-24 11:28:18 +0100

    add parameterIOHandler class

---
+++ src/db/ParameterIOHandler.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.util.Hashtable;
+
+public class ParameterIOHandler {
+	
+	//serializes the given hash and save it to the given path
+	public static void saveParameters(String path, Hashtable<String, Integer> params){
+		
+	}
+	
+	
+	
+	//looks for a serialized hash on the given path, de-serializes and returns it
+	public static Hashtable<String, Integer> loadParameters(String path){
+		Hashtable<String, Integer> res = null;
+		
+		
+		
+		return res;
+	}
+	
+	
+}
