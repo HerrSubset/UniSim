0bf669b71e9bbd675019bf54083a177984cff285
HerrSubset <smets.pj@gmail.com>
2015-12-31 11:37:23 +0100

    make entities section display a list

--- src/gui/UnitList.java
+++ src/gui/UnitList.java
@@ -1,18 +1,28 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.BorderFactory;
-import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.Border;
 
 @SuppressWarnings("serial")
 public class UnitList extends JPanel {
 
 	public UnitList(){
+		this.setLayout(new BorderLayout());
+		String[] sList = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien"};
+		
+		JList<String> list = new JList<String>(sList);
+
+		JScrollPane scroller = new JScrollPane(list);
+		this.add(scroller, BorderLayout.CENTER);
+		
 		Border b = BorderFactory.createTitledBorder("Entity List");
 		this.setBorder(b);
-		
-		JLabel label = new JLabel("this is where the list goes");
-		this.add(label);
+		this.setPreferredSize(new Dimension(150, 300));
 	}
 }
